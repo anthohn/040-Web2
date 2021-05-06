0545cef6280e3c4cdab7064760cb81917d27a63f
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2021-05-06 18:28:21 +0200

    404 error

---
+++ src/php/404.php
@@ -0,0 +1,9 @@
+<?php $title = 'Erreur 404';
+require "template/header.php"; ?>
+
+<div class="error404">
+    <h1>Erreur 404</h1>
+    <p>Oops, cette page n'existe pas !</p>
+</div>
+
+<?php require "template/footer.php"; ?>
