7cecb064b6dd496ed66f04596734c5c817d19a47
Anthony Höhn <anthonyhohn@gmail.com>
2021-05-18 14:20:07 +0200

    modif livre

--- src/php/editBook.php
+++ src/php/editBook.php
@@ -13,6 +13,7 @@ else
     $books = $db->getBook($idBook);
     $bddNotes = $db->getNotesBook($idBook)[0]['votNote'];
     $_SESSION['bookNoteAvg'] = $idBook;    
+    $authors = $db->getAuthor();
 }
 
 // Si l'utilisateur est connecté récupere l'id de session dans $idUser
@@ -56,20 +57,23 @@ if(isset($_POST['submit']))
 
 
 <div class="MainDetailBookBlock">
-    <?php foreach($books as $book): ?>
-        <h1>Appréciation / Détail
-            <?php if(isLogged() && (isAdmin())): ?>
-                <a href="editBook.php?idBook=<?= $book["idBook"]; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg></a>
-                <a href="deleteBook.php?idBook=<?= $book["idBook"]; ?>" onclick="return confirm('Êtes vous sûr de vouloir supprimer le livre ?')"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg></a>
-            <?php endif; ?> 
-        </h1>
+<?php foreach($books as $book): ?> 
+    <h1>Modification <?= $book['booTitle'] ?></h1>
         <div class="bookDetailBlock">            
             <img src="../../resources/images/books/<?= $book['idBook'];?>.jpg" alt="première de couverture"/>   
+
             <div class="bookContent">          
-                <h2><?= $book['booTitle'] ?></h2>
-                <h3><?= $book['autLastname'] ?> <?= $book['autFirstname'] ?></h3>
+                <h2><input type="text" value="<?= $book['booTitle'] ?>"></h2>
+                
+                <h3><select name="type" id="type">
+                        <option value='<?= $book["idAuthor"];?>'><?= $book["autLastname"] . ' ' . $book["autFirstname"];?></option>
+                        <?php foreach($authors as $author) : ?>
+                            <option value="<?= $author["idAuthor"]; ?>"><?= $author["autLastname"] . ' ' . $author["autFirstname"]; ?></option>
+                        <?php endforeach; ?>
+                    </select></h3>
+
                 <h4>Résumé : </h4> 
-                <p class="booSummary"><?= $book['booSumary'] ?></p> 
+                <p class="booSummary"><textarea id="w3review" name="w3review" rows="3" cols="137"><?= $book['booSumary'] ?></textarea></p> 
                 <p id="catPages"><?= $book['catName'] ?> - <?= $book['booPages'] ?> pages</p> 
                 <p id="editorPubliYear"><?= $book['ediName'] ?> - <?= $book['booPublicationYear'] ?></p>
                 <!-- <p>< //$book['booSumary'] </p> 
