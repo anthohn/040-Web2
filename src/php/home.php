334f896e9932c78783f96b3341a241f497eb69cf
Younes Sayeh <yousayeh@etmlnet.local>
2021-05-26 10:13:10 +0200

    Fixe des moyennes de note pour les 5 derniers livres ajoutés

--- src/php/home.php
+++ src/php/home.php
@@ -1,7 +1,7 @@
 <?php 
 $title = 'Accueil';
 require ('template/header.php');
-$fiveBooks = $db->LastFiveBooks();
+$fiveBooksNotes = $db->lastFiveBooks();
 ?>
 
 <div class="content">
@@ -26,21 +26,25 @@ $fiveBooks = $db->LastFiveBooks();
     </div>
 
     <div class="mainBookblock">
-        <?php foreach ($fiveBooks as $fiveBook) : ?>
+        <?php foreach($fiveBooksNotes as $fiveBooksNote) : ?>
             <div class="bookBlock">
                 <div class="bookImage">
-                    <a href="details.php?idBook=<?= $fiveBook['idBook'];?>"><img class="imageBook"  src="../../resources/images/books/<?= $fiveBook['idBook'];?>.jpg" alt="première de couverture"/></a>
+                    <a href="details.php?idBook=<?= $fiveBooksNote['idBook'];?>"><img class="imageBook"  src="../../resources/images/books/<?= $fiveBooksNote['idBook'];?>.jpg" alt="première de couverture"/></a>
                 </div>
                 <div class="middle">
-                    <div class="zoom"><a href="details.php?idBook=<?= $fiveBook['idBook'];?>"><svg class="zoomIcon" width="30" height="30" fill="currentColor" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></a></div>
+                    <div class="zoom"><a href="details.php?idBook=<?= $fiveBooksNote['idBook'];?>"><svg class="zoomIcon" width="30" height="30" fill="currentColor" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></a></div>
                 </div>
                 <div class="bookInfo">
-                    <p id="bookTitle"><?= $fiveBook['booTitle'] ?></p> 
-                    <p id="bookAuthor"><?= $fiveBook['autFirstname'] ?></p> 
-                    <p id="bookAvg"><?= $fiveBook['votNote'] ?> / 5</p>
+                    <p id="bookTitle"><?= $fiveBooksNote['booTitle'] ?></p> 
+                    <p id="bookAuthor"><?= $fiveBooksNote['autFirstname'] ?></p> 
+                    <?php if(empty($fiveBooksNote['votNote'])): ?>
+                        <p id="bookAvg">0 / 5</p>
+                    <?php else: ?>
+                        <p id="bookAvg"><?= $fiveBooksNote['votNote'] ?> / 5</p>
+                    <?php endif; ?>
                 </div>
             </div>
-        <?php endforeach ?>
+        <?php endforeach; ?>
     </div>
 </div>
 <?php require ('template/footer.php'); ?>
